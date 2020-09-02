9fb16607b13002e18ee3363395bc26411ac19de0
Junha Park <48537888+park-junha@users.noreply.github.com>
2020-09-02 00:04:26 -0700

    (0.0.1) Modify sample ScrumifyLabels data (#1)
    * Modify sample ScrumifyLabels data
    
    * Tweak color of In Progress labels
    
    * Reword CHANGELOG

--- 1.0/postgres/data/POPULATE_SCRUMIFY_LABELS.sql
+++ 1.0/postgres/data/POPULATE_SCRUMIFY_LABELS.sql
@@ -57,7 +57,7 @@ VALUES
 ),
 (
   'Story: 13 Points',
-  '3572ae',
+  'b845fc',
   'This task is very large and may need to be broken down into smaller stories'
 ),
 (
@@ -87,7 +87,7 @@ VALUES
 ),
 (
   'In Progress',
-  'f9a602',
+  'f0db4f',
   'This task is currently being worked on'
 ),
 (
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 Version log for Spekkio Database Schema.
 
+## 0.0.1
+### Modified
+- Recolored 13-point and in-progress labels in ScrumifyLabels sample data
+
 ## 0.0.0
 ### New
 - Initial commit
